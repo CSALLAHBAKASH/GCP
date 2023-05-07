a5023f0b0501531892789871455db83225979b03
khajasab <khajasabengr@gmail.com>
2023-05-07 14:00:22 +0530

    enable services udpated

---
+++ GCP_services.sh
@@ -0,0 +1,4 @@
+!gcloud services enable pubsub.googleapis.com
+!gcloud services enable cloudfunctions.googleapis.com
+!gcloud services enable cloudbuild.googleapis.com
+!gcloud services enable sourcerepo.googleapis.com
