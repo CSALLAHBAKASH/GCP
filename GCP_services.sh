c31046d1dd65c0e96e729401cfa10d2dbd5fea62
khajasab <khajasabengr@gmail.com>
2023-05-07 18:24:12 +0530

    services enables

--- GCP_services.sh
+++ GCP_services.sh
@@ -2,3 +2,9 @@
 !gcloud services enable cloudfunctions.googleapis.com
 !gcloud services enable cloudbuild.googleapis.com
 !gcloud services enable sourcerepo.googleapis.com
+!gcloud services enable \
+  containerregistry.googleapis.com \
+  cloudbuild.googleapis.com \
+  secretmanager.googleapis.com \
+  container.googleapis.com \
+  ml.googleapis.com
